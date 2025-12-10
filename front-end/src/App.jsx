0eaa2dbd4816d61a6705bd124760f52141f8aae9
ntxtthomas <apm.tthomas@gmail.com>
2025-12-10 17:09:54 -0600

    01_03_end

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './pages/HomePage.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <HomePage />
+  );
+}
+
+export default App
