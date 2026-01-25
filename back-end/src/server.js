e01e0f77495b321014fe32dbfb90115166beb7f2
ntxtthomas <apm.tthomas@gmail.com>
2026-01-25 09:57:57 -0600

    nodemon and route params

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,8 +3,21 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
 app.get('/hello', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World from a GET endpoint!');
+});
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}, from a GET endpoint with a parameter!`);
+});
+
+// app.get('/goodbye', (req, res) => {
+//   res.send('Goodbye, World from a GET endpoint!');
+// });
+app.post('/hello', (req, res) => {
+  res.send('Hello, ' + req.body.name + ', from a POST endpoint!');
 });
 
 app.listen(PORT, () => {
