0d90d2e2b1d2ea41db5c6ffd4aec6e0ce83c5cf6
ntxtthomas <apm.tthomas@gmail.com>
2026-02-04 12:48:42 -0600

    login logout UI

--- front-end/src/navBar.jsx
+++ front-end/src/navBar.jsx
@@ -1,12 +1,31 @@
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+  
 export default function NavBar() {
+  const navigate = useNavigate();
+  const isLoggedIn = false;
+  const email = "user@example.com";
+
   return (
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/articles">Articles</Link></li>
+        <li>
+          {isLoggedIn && (
+            <li>
+              Logged in as: {email}
+            </li>
+          )}
+        </li>
+        <li>
+          {isLoggedIn 
+          ? <button onClick= {() => signOut(getAuth())}>Log Out</button>
+          : <button onClick={() => navigate("/login")}>Log In</button>
+          }
+        </li>
       </ul>
     </nav>
   );
